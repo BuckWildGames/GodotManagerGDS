ef8bed82df03c8f2a103eaec266ed2925b2ccba3
Buck Wild Games <96750661+BuckWildGames@users.noreply.github.com>
2025-12-04 09:21:29 -0700

    Added version control

---
+++ addons/version_control/plugin.gd
@@ -0,0 +1,16 @@
+@tool
+extends EditorPlugin
+
+const VersionControlDock = preload("assets/version_control_dock.tscn")
+
+var dock: Control
+
+func _enter_tree():
+	dock = VersionControlDock.instantiate()
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BR, dock)
+
+
+func _exit_tree():
+	if dock:
+		remove_control_from_docks(dock)
+		dock.free()
